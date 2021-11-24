3272a37a06ea2ffe134c51c7a5efa109ee3bc36b
Marcin <delektowski@interia.pl>
2021-11-24 18:06:10 +0100

    Angular FAST implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,47 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {
+  provideFASTDesignSystem,
+  fastCard,
+  fastButton,
+  fastTextField,
+  buttonStyles,
+  fastRadio,
+  fastRadioGroup,
+  radioStyles
+} from '@microsoft/fast-components';
+import { css } from '@microsoft/fast-element';
 
 if (environment.production) {
   enableProdMode();
 }
+
+provideFASTDesignSystem().register(
+  fastCard(),
+  fastButton({
+    styles: (ctx, def) => css`
+      ${buttonStyles(ctx, def as any)}
+      :host([appearance="accent"]) button {
+        background: blue;
+      }
+    `,
+  }),
+  fastTextField(),
+  fastRadio({
+    styles: (ctx, def) => css`
+      ${  radioStyles
+    (ctx, def as any)}
+      :host([aria-checked="true"]) .control {
+        background: blue;
+      }
+      :host([aria-checked="true"]:not([disabled])) .control:hover {
+        background: blue;
+      }
+    `,
+  }),
+  fastRadioGroup()
+);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
